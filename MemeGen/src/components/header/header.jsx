5583c800394e47d917a4ed20038d8f99d04fa401
Tommyhh04 <tommyhh04.kodehode@gmail.com>
2024-01-15 12:07:49 +0100

    added image and css to header element

--- MemeGen/src/components/header/header.jsx
+++ MemeGen/src/components/header/header.jsx
@@ -1,9 +1,13 @@
 import "./header.css";
+import Trollface from "../Images/Trollface.png";
 
 export default function Header() {
   return (
     <div className="header">
-      <h1>Meme Generator</h1>
+      <div className="header--title">
+        <img src={Trollface}></img>
+        <h1>Meme Generator</h1>
+      </div>
       <h3>React Course - Project 3</h3>
     </div>
   );
