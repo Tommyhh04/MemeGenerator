398ccf320caa482eb9aeba5e954f333994915458
Tommyhh04 <tommyhh04.kodehode@gmail.com>
2024-01-15 13:47:32 +0100

    Added more components and css

--- MemeGen/src/App.jsx
+++ MemeGen/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/header";
 import UserInput from "./components/input/input";
+import Display from "./components/display/MemeDisplay";
 
 function App() {
   return (
     <div className="box">
       <Header />
       <UserInput />
+      <Display />
     </div>
   );
 }
---
+++ MemeGen/src/components/display/MemeDisplay.jsx
@@ -0,0 +1,13 @@
+/*Displays the meme image with the added text from  user input*/
+import Grafild from "../Images/Grafild.jpg";
+import "./display.css";
+
+export default function Display() {
+  return (
+    <div className="Display--container">
+      <div className="Display">
+        <img className="Meme--Img" src={Grafild}></img>
+      </div>
+    </div>
+  );
+}
