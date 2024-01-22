c11298d38c34fa4988043bd6f10033b3d897e8ef
Eggcanoe <tommyhalleland@hotmail.com>
2024-01-22 10:18:44 +0100

    ran audit fix for security bug. added comment

--- MemeGen/src/components/display/MemeDisplay.jsx
+++ MemeGen/src/components/display/MemeDisplay.jsx
@@ -1,13 +1,13 @@
-/*Displays the meme image with the added text from  user input*/
 import Grafild from "../Images/Grafild.jpg";
 import "./display.css";
 
+/*Displays the meme image with the added text from  user input*/
 export default function Display() {
   return (
     <div className="Display--container">
       <div className="Display">
+        {/*Displays the meme*/}
         <img className="Meme--Img" src={Grafild}></img>
-        <h1>Bottom text</h1>
       </div>
     </div>
   );
