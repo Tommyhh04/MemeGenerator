abe1cc87b1c7d0727c92ca0ab69abcc9b9e2f74f
Eggcanoe <tommyhalleland@hotmail.com>
2024-03-04 09:32:12 +0100

    Updated function getMemeImage, added comments

--- MemeGen/src/components/MemeCreator/input.jsx
+++ MemeGen/src/components/MemeCreator/input.jsx
@@ -2,26 +2,41 @@ import React from "react";
 import "./input.css";
 import memesData from "../memes/memesData";
 
+//This is the base of the generator
 export default function UserInput() {
-  const [memeImage, setMemeImage] = React.useState("");
-  // Function to get a random meme image
+  //Saves the meme related data as an object called "meme"
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+  // Function to get a random meme image from a API or local stored data file
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
-
+  //Displays the buttons
   return (
     <main>
       <div className="userInput">
         <div className="userInput--forms">
+          {/*Text boxes for where the user can type in the top and bottom text that is used in the meme*/}
           <input type="text" placeholder="Top text"></input>
           <input type="text" placeholder="Bottom text"></input>
         </div>
         <button onClick={getMemeImage}>Get a new meme image 📦</button>
       </div>
+      {/*Displays the meme*/}
       <div className="image--container">
-        <img src={memeImage} className="meme--mage"></img>
+        <img src={meme.randomImage} className="meme--mage"></img>
       </div>
     </main>
   );
--- MemeGen/src/components/display/MemeDisplay.jsx
+++ MemeGen/src/components/display/MemeDisplay.jsx
@@ -1,6 +1,6 @@
 import Grafild from "../Images/Grafild.jpg";
 import "./display.css";
-//Not currently in use
+// !!! THIS CODE IS NOT IN USE RIGHT NOW. MAYBE TO BE REMOVED !!!
 /*Displays the meme image with the added text from  user input*/
 export default function Display() {
   return (
